0be6c79214b539edb2bcb65a7e023eddb510d436
Aadiz Dagar <aadi@Aadizs-MacBook-Pro.local>
2024-03-17 23:42:04 +1030

    Submit

--- Truckloads.cpp
+++ Truckloads.cpp
@@ -5,10 +5,10 @@ int Truckloads::numTrucks(int numCrates, int loadSize) {
         return 1; // Base case: all crates fit in one truckload
     } else {
         // Calculate the number of full truckloads
-        int fullTrucks = numCrates / loadSize;
+        int fullTrucks = numCrates % loadSize;
 
         // Calculate the number of remaining crates after filling full truckloads
-        int remainingCrates = numCrates / loadSize;
+        int remainingCrates = numCrates % loadSize;
 
         // If there are remaining crates, add one more truck
         if (remainingCrates > 0) {
